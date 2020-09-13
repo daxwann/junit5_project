87b1c7cd1d1f1f6e3272ca194c5ef78f7519d79c
Dax Wann <daxwann@gmail.com>
2020-09-12 17:49:12 -0700

    add jupiter dependencies and test structure

---
+++ src/test/java/junitbasics/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package junitbasics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+  @Test
+  void add() {
+    System.out.println("this test ran");
+  }
+}
